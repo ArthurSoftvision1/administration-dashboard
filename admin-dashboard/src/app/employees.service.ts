77184401408b76812533010595705564b7920acb
ArthurSoftvision1 <molnar_arthur@yahoo.com>
2023-10-26 16:24:06 +0300

    create employees Service with getEmployees & getShifts methods

---
+++ admin-dashboard/src/app/employees.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as dbData from '@insightfulio/insightful-test-api-server/db.json';
+import { catchError, map, Observable, of } from 'rxjs';
+import { Employee } from './_models/employee.interface';
+import { EmployeeData } from './_models/employees.interface';
+import { Shift } from './_models/shift.interface';
+import { ShiftData } from './_models/shifts.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeesService {
+  private data: any; // Store the JSON data here
+
+  constructor(private http: HttpClient) {
+    this.data = dbData;
+  }
+
+  getEmployees(): Observable<Employee[]> {
+    // If data is already loaded, return it
+    if (this.data) {
+      return of(this.data.employees);
+    } else {
+      // Fetch and store the data, then return employees
+      return this.http.get<EmployeeData>('@insightfulio/insightful-test-api-server/db.json').pipe(
+        map(data => {
+          this.data = data;
+          return data.employees;
+        }),
+        catchError(error => {
+          console.error('Error fetching data:', error);
+          return of([]);
+        })
+      );
+    }
+  }
+
+  getShifts(): Observable<Shift[]> {
+    // If data is already loaded, return it
+    if (this.data) {
+      return of(this.data.shift);
+    } else {
+      // Fetch and store the data, then return shifts
+      return this.http.get<ShiftData>('@insightfulio/insightful-test-api-server/db.json').pipe(
+        map(data => {
+          this.data = data;
+          return data.shifts;
+        }),
+        catchError(error => {
+          console.error('Error fetching data:', error);
+          return of([]);
+        })
+      );
+    }
+  }
+}
